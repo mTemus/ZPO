07bc5f9c11e737cc773de24a7c666a09abf85944
Temus <temus@wir.pl>
2019-03-28 16:55:57 +0100

    v0.0.3

--- LAB 01 04.03.2019/src/main/java/Main.java
+++ LAB 01 04.03.2019/src/main/java/Main.java
@@ -4,10 +4,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("src/java/view/employeeView.fxml"));
         primaryStage.setTitle("Panel administratora");
         primaryStage.setScene(new Scene(root));
@@ -16,6 +18,24 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+
+        try {
+            Connection connMYSQL = DriverManager.getConnection("jdbc:mysql://localhost:3306/zpo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "admin");
+
+            Statement myStatement = connMYSQL.createStatement();
+            ResultSet myResultSet = myStatement.executeQuery("select * from employee");
+
+            while (myResultSet.next()) {
+                System.out.println(myResultSet.getString("id") + ", " + myResultSet.getString("name"));
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
+
 }
+
---
+++ LAB 01 04.03.2019/src/main/java/view/Connector.java
@@ -0,0 +1,19 @@
+package view;
+
+import java.sql.*;
+
+public class Connector {
+
+    {
+        try {
+            Connection connMYSQL = DriverManager.getConnection("jdbc:mysql://localhost:3306/zpo", "root", "admin");
+            Statement myStatement = connMYSQL.createStatement();
+            ResultSet myResultSet = myStatement.executeQuery("select * from employee");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
