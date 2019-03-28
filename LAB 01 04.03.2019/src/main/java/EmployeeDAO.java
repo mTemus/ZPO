7551faaa3f8f2e30dbbb235564b907d37262ff82
Temus <temus@wir.pl>
2019-03-28 16:19:26 +0100

    v0.0.2

--- LAB 01 04.03.2019/src/main/java/EmployeeDAO.java
+++ LAB 01 04.03.2019/src/main/java/EmployeeDAO.java
@@ -1,14 +1,12 @@
-package java;
-
 public interface EmployeeDAO {
-    /** * Zwraca pracownika o podanym id */
-    Optional findOne(Integer id);
-    /** * Zwraca wszystkich pracowników */
-    List findAll();
-    /** * Zwraca pracownika o podanym nazwisku */
-    Optional findByName(String name);
-    /** * Usuwa pracownika */
-    void delete(Employee employee);
-    /** * Dodaje, jeśli brak, lub aktualizuje pracownika */
-    void save(Employee employee);
+//    /** * Zwraca pracownika o podanym id */
+//    Optional findOne(Integer id);
+//    /** * Zwraca wszystkich pracowników */
+//    List findAll();
+//    /** * Zwraca pracownika o podanym nazwisku */
+//    Optional findByName(String name);
+//    /** * Usuwa pracownika */
+//    void delete(Employee employee);
+//    /** * Dodaje, jeśli brak, lub aktualizuje pracownika */
+//    void save(Employee employee);
 }
