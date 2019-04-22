d5c35b76f4f2b0aa8ce7c8a239b689b08ec7d796
Temus <temus@wir.pl>
2019-04-22 19:34:29 +0200

    v0.0.6

--- LAB 04 15.04.2019/src/main/java/controller/ReflectionController.java
+++ LAB 04 15.04.2019/src/main/java/controller/ReflectionController.java
@@ -3,17 +3,18 @@ package controller;
 import classes.menu.Pizza;
 import classes.storage.Item;
 import classes.waiting_room.Customer;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class ReflectionController {
 
 
     public TextField class_name_field;
-    public ComboBox classes_combobox;
+    public TextArea class_methods_textarea;
+    public TextField class_methods_textfield;
 
 
-    public void initialize(){
+    public void initialize() {
         showAllClasses();
     }
 
@@ -23,12 +24,11 @@ public class ReflectionController {
         Class reflectItem = Item.class;
         Class reflectCustomer = Customer.class;
 
-        classes_combobox.getItems().addAll(
-                reflectPizza.getName(),
-                reflectItem.getName(),
-                reflectCustomer.getName()
-        );
-
+        class_methods_textfield.setText("Available classes:");
+        class_methods_textarea.setText(
+                reflectPizza.getName() + "\n" +
+                reflectItem.getName() + "\n" +
+                reflectCustomer.getName());
 
 
     }
