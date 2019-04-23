f7faa1f41f826cfe49fd71a7433236be26c5759b
Temus <temus@wir.pl>
2019-04-23 14:17:54 +0200

    v0.1

--- LAB 04 15.04.2019/src/main/java/controller/ReflectionController.java
+++ LAB 04 15.04.2019/src/main/java/controller/ReflectionController.java
@@ -3,8 +3,12 @@ package controller;
 import classes.menu.Pizza;
 import classes.storage.Item;
 import classes.waiting_room.Customer;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.lang.reflect.Method;
 
 public class ReflectionController {
 
@@ -12,26 +16,78 @@ public class ReflectionController {
     public TextField class_name_field;
     public TextArea class_methods_textarea;
     public TextField class_methods_textfield;
+    public Button use_class_button;
 
+    private Class reflectPizza = Pizza.class;
+    private Class reflectItem = Item.class;
+    private Class reflectCustomer = Customer.class;
+
+    private Object chosedClassObject = null;
+
+    Method[] pizzaMethods = reflectPizza.getMethods();
+    Method[] itemMethods = reflectItem.getMethods();
+    Method[] customerMethods = reflectCustomer.getMethods();
+
+
+    public void useClass(ActionEvent event) {
+        String className = class_name_field.getText();
+        createObjectOfClass(className);
+    }
 
     public void initialize() {
         showAllClasses();
     }
 
-    public void showAllClasses() {
-
-        Class reflectPizza = Pizza.class;
-        Class reflectItem = Item.class;
-        Class reflectCustomer = Customer.class;
-
+    private void showAllClasses() {
         class_methods_textfield.setText("Available classes:");
         class_methods_textarea.setText(
                 reflectPizza.getName() + "\n" +
-                reflectItem.getName() + "\n" +
-                reflectCustomer.getName());
+                        reflectItem.getName() + "\n" +
+                        reflectCustomer.getName()
+        );
+    }
+
+    private void createObjectOfClass(String className) {
+        try {
+            chosedClassObject = Class.forName(className).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (chosedClassObject != null){
+            Method[] methodsOfChosenClass = chosedClassObject.getClass().getDeclaredMethods();
+            showAllMethods(methodsOfChosenClass);
+        }
+        else class_methods_textfield.setText("Wrong class name!");
 
 
     }
+
+    public void showAllClassFields() {
+
+    }
+
+    private void showAllMethods(Method[] methodsOfChosenClass) {
+        StringBuilder allMethods = null;
+        String getterMethods = null;
+
+        for (Method m : methodsOfChosenClass) {
+            allMethods.append(m.getName()).append("\n");
+        }
+
+        for (Method m : methodsOfChosenClass) {
+            allMethods.append(m.getName().contains("get")).append("\n");
+        }
+
+
+        class_methods_textfield.setText("Available methods:");
+        class_methods_textarea.setText(allMethods.toString());
+    }
+
 
 
 }
