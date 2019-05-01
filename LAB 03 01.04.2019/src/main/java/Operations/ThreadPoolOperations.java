ce471f7fa31aa8f967e6a867a77f77aa2179b42b
Temus <temus@wir.pl>
2019-05-01 12:57:08 +0200

    v6.2

--- LAB 03 01.04.2019/src/main/java/Operations/ThreadPoolOperations.java
+++ LAB 03 01.04.2019/src/main/java/Operations/ThreadPoolOperations.java
@@ -9,28 +9,54 @@ import java.util.concurrent.Executors;
 public class ThreadPoolOperations {
     private static double now = 0;
     private static double previous;
-    private static int poolSize = 5;
+    private static int poolSize = 6;
+    int queue = 0;
+    int producerId = 0;
+    int consumerId = 0;
     private TimeOperations TO = new TimeOperations();
 
     public void runThreadPool(List<Item> items) {
         previous = System.nanoTime();
         ExecutorService threadsManager = Executors.newFixedThreadPool(poolSize);
 
-        for (int i = 0; i < 5; i++) {
-            int finalI = i;
+        for (int i = 0; i < poolSize; i++) {
             threadsManager.execute(new Thread(() -> {
-                multiThreadProduce(finalI, items, poolSize);
-                multiThreadConsume(finalI, items, poolSize);
+                try {
+                    manageThreads(items, poolSize);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
             ));
         }
 
-        if (threadsManager.isTerminated())
-            threadsManager.shutdown();
-        while (!threadsManager.isShutdown())
-            now = System.nanoTime();
+//        while (threadsManager.isTerminated()) {
+//            if (threadsManager.isTerminated()) {
+//                threadsManager.shutdown();
+//                now = System.nanoTime();
+//            }
+//        }
+
         TO.showActionTime(previous, now);
     }
+
+    private void manageThreads(List<Item> items, int size) throws InterruptedException {
+        size /= 2;
+        if (queue > 1) {
+            queue = 0;
+        }
+        if (queue == 0) {
+            ++queue;
+            multiThreadProduce(producerId++, items, size);
+        } else if (queue == 1) {
+            ++queue;
+            Thread.sleep(1);
+            multiThreadConsume(consumerId++, items, size);
+        }
+
+
+    }
+
 
     private static void multiThreadProduce(int singleThreadID, List<Item> items, int size) {
         System.out.println(Thread.currentThread().getName());
